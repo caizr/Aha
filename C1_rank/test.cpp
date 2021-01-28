0fce0f1ad7726ebd262c110c6442c9f024833ba0
caizr <0@caizr.com>
2021-01-28 13:09:27 +0800

    a

---
+++ C1_rank/test.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+// decleration
+int swap(int*,int*);
+using namespace std;
+ int main(){
+     int a[10]={1,2,4,5,6,7,8,9,11,12};
+
+     for(int i=0;i<10-1;++i){
+         for (int j=0;j<10-1;++j){
+             if ( a[j] <a[j+1])
+                swap( a+j, a+j+1 );
+         }
+     }
+     for (int i =0;i<10;++i){
+         cout<< a[i]<<endl;
+     }
+     cout<<"hello "<<endl;
+     return 0;
+ }
+ int swap(int* a,int* b){
+     int temp = *a;
+     *a=*b;
+     *b=temp;
+     return 1;
+
+ }
